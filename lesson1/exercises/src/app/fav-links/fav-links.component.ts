66ef36ce9b2c682703048d77527b46a5568b27e1
Fred Allen <allenf3@msn.com>
2020-12-07 16:26:20 -0600

    Complete

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fav-links',
+  templateUrl: './fav-links.component.html',
+  styleUrls: ['./fav-links.component.css']
+})
+export class FavLinksComponent implements OnInit {
+  favLinks = ["https://www.cnn.com", "https://www.cnbc.com"];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
