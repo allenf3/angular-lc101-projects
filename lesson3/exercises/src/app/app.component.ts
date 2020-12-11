211de8618bf2cc2351f1bcfdae1e47c6651216a9
Fred Allen <allenf3@msn.com>
2020-12-11 17:25:54 -0600

    Lesson 3 exercises complete

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Attribute, Component } from '@angular/core';
+import { runInThisContext } from 'vm';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,98 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Exercises: Angular Lesson 3';
+  title: string = 'Exercises: Angular Lesson 3';
+  ready: boolean = false;
+  confirmAbort: boolean = false;
+  takeOffEnabled: boolean = true;
 
-  color = 'green';
-  height = 0;
-  width = 0;
-  message = 'Space shuttle ready for takeoff!';
+  color: string = 'green';
+  height: number = 0;
+  width: number = 0;
+  message: string = 'Space shuttle ready for takeoff!';
+  upButton: boolean = true;
+  downButton: boolean = true;
+  leftButton: boolean = true;
+  rightButton: boolean = true;
+
+
+
+  fly() {
+    this.ready = confirm("Confirm that the shuttle is ready for takeoff.")
+    if(this.ready) {
+      this.message = "Shuttle in flight";
+      this.color = "blue";
+      this.height += 10000;
+      this.takeOffEnabled = false;
+    }
+  }
+
+  land(rocketImage: HTMLElement) {
+    alert("The shuttle is landing. Landing gear engaged.");
+    this.message = "The shuttle has landed.";
+    this.color = "green";
+    this.height = 0;
+    rocketImage.style.bottom = '0px';
+    this.takeOffEnabled = true;
+  }
+
+  abort(rocketImage: HTMLElement) {
+    this.confirmAbort = confirm("Are you sure you want to abort the mission?");
+    if(this.confirmAbort) {
+      this.message = "Mission aborted."
+      this.color = "red";
+      this.height = 0;
+      rocketImage.style.bottom = '0px';
+      this.confirmAbort = false;
+      this.takeOffEnabled = true;
+    }
+  }
+
+  moveRocket(rocketImage: HTMLElement, direction: string) {
+    if(direction === 'right') {
+      this.leftButton = true;
+      let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width + 10000;
+      this.tooClose();
+      if(this.width > 460000) {
+        this.rightButton = false;
+      }
+    } else if (direction === 'left') {
+      this.rightButton = true;
+      let movement = parseInt(rocketImage.style.left) - 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width - 10000;
+      this.tooClose();
+      if(this.width < -10000) {
+        this.leftButton = false;
+      }
+    } else if (direction === 'up') {
+      this.downButton = true;
+      let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height = this.height + 10000;
+      this.tooClose();
+      if(this.height > 330000) {
+        this.upButton = false;
+      }
+    } else if (direction ==='down') {
+      this.upButton = true;
+        let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
+        rocketImage.style.bottom = movement;
+        this.height = this.height - 10000;
+        this.tooClose();
+        if(this.height < 20000) {
+          this.downButton = false;
+        }
+    }
+    
+  }
+  tooClose() {
+    if(this.height > 320000 || this.height < 30000 || this.width < 0 || this.width > 450000) {
+      this.color = "black";
+    } else {
+      this.color = "blue";
+    }
+  }
 }
